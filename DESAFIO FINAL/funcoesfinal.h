07f2fcbb9ec18ed606b875e736b7ded61b23bac9
mairacanal <61601791+mairacanal@users.noreply.github.com>
2020-04-10 10:04:23 -0300

    Update funcoesfinal.h

--- DESAFIO FINAL/funcoesfinal.h
+++ DESAFIO FINAL/funcoesfinal.h
@@ -10,15 +10,10 @@ extern "C" {
 
 //SOMA
 //type soma (type)(int N, type vector[])
-#define declare_soma(T)\
-T soma (T)(int N, T vector[]);
+#define soma(T) soma_##T
 
-#define define_soma(T)\
-T soma (T)(int N, T vector[]){\
-    T res_soma = 0;\
-    for (int i = 0; i < N; i++) res_soma = res_soma + vector[i];\
-    return (res_soma);\
-}
+#define declare_soma(T)\
+T soma(T)(int N, T vector[]);
 
 declare_soma(int);
 declare_soma(double);
@@ -27,17 +22,10 @@ declare_soma(float);
 
 //MÉDIA
 //type media (type)(int N, type vector[])
-#define declare_media(T)\
-T media (T)(int N, T vector[]);
+#define media(T) media_##T
 
-#define define_media(T)\
-T media (T)(int N, T vector[]){\
-    T res_soma = 0;\
-    for (int i = 0; i < N; i++) res_soma = res_soma + vector[i];\
-    T res_med;\
-    res_med = (T) res_soma/N;\
-    return (res_med);\
-}
+#define declare_media(T)\
+T media(T)(int N, T vector[]);
 
 declare_media(int);
 declare_media(double);
@@ -46,42 +34,22 @@ declare_media(float);
 
 //DESVIO PADRÃO
 //type desvio_padrao (type)(int N, T vector[])
-#define declare_desvp(T)\
-T desvio_padrao (T)(int N, T vector[]);
+#define desvio_padrao(T) desvio_padrao_##T
 
-#define define_desvp(T)\
-T desvio_padrao (T)(int N, T vector[]){\
-    T res_soma = 0;\
-    for (int i = 0; i < N; i++) res_soma = res_soma + vector[i];\
-    T res_med;\
-    res_med = (T) res_soma/N;\
-    T res_devp;\
-    T soma_dev = 0;\
-    for (int i = 0; i < N; i++) soma_dev = soma_dev + ((vector[i] - res_med)*(vector[i] - res_med));\
-    res_devp = sqrt((float) soma_dev/N);\
-    return (res_devp);\
-}
+#define declare_desvio_padrao(T)\
+T desvio_padrao(T)(int N, T vector[]);
 
-declare_desvp(int);
-declare_desvp(float);
-declare_desvp(double);
+declare_desvio_padrao(int);
+declare_desvio_padrao(float);
+declare_desvio_padrao(double);
 //----------------------------------------
 
 //DIVISÃO
 //int divisao (int n[3], T* res)
-#define declare_divisao(T)\
-int divisão (T)(int n[3], T* res);
+#define divisao(T) divisao_##T
 
-#define define_divisao(T)\
-int divisão (T)(int n[3], T* res){\
-    if (n[2] == 0) return (-1);\
-    else{\
-        *res = (T) n[0]/n[2];\
-        res++;\
-        *res = (T) n[1]/n[2];\
-        return 0;\
-    }\
-}
+#define declare_divisao(T)\
+int divisão(T)(int n[3], T* res);
 
 declare_divisao(int);
 declare_divisao(float);
